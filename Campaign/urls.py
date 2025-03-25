d0fb6d5f6c278b179f2a8ded2bcc2d75d94c671b
fpsnetodf <fps_netodf@hotmail.com>
2025-03-25 16:52:00 -0300

    2 alteração

--- Campaign/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('agendas/', views.lista_agendas, name='lista_agendas'),
-    path('agendas/nova/', views.criar_agenda, name='criar_agenda'),
-]
